8fc99c491b9c7aad5fba2b6503810a73315c6abe
opilane <opilane@HP-CZC2349HTM>
2024-01-31 11:18:34 +0200

    Add project files.

---
+++ Kordused.py
@@ -0,0 +1,58 @@
+from random import *
+
+#1
+z = "   (\_/)\n   (o o)\n   / | \*"
+arv = int(input("mitu "))
+for i in range(0, 9):
+    for j in range(0, arv):
+        print(z)
+    print()
+
+#2
+sum = 0
+r = int(input("mitu"))
+for i in range(0, r):
+    sum += i
+
+print(sum)
+
+#3
+rand = randint(1,100)
+print(rand)
+for i in range(0, 10):
+    vastus = int(input("proovi ära arvata 10 katse arv. "))
+    if vastus == rand:
+        print("jah!")
+        break
+    else:
+        print("ei!")
+        
+#4
+#v1
+arv = str(input("mitu "))
+summ = ""
+for i in reversed(arv):
+    summ += i
+print(summ)        
+
+#v2
+arv = int(input("mitu "))
+arv1 = 0
+summ = ""
+
+while arv > 0:
+    digit = arv % 10
+    arv = arv // 10
+    summ += str(digit)
+
+print(summ)
+
+#5
+arv = str(input("mitu "))
+summ = 0
+urutamine = 1
+for i in arv:
+    summ += int(i)
+    urutamine *= int(i)
+
+print(summ, urutamine)

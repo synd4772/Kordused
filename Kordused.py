74b8edb87c78ca14b88651cc3e3668067b79d90d
opilane <opilane@HP-CZC2349HTM>
2024-02-02 10:25:12 +0200

    uus

--- Kordused.py
+++ Kordused.py
@@ -1,6 +1,21 @@
 ﻿from random import *
 
-#1
+
+while True:
+    try:
+        K=int(input("Mitu tk: "))
+        if K>0: break
+    except ValueError:
+        print("Vale tüüp")
+while True: 
+    try:
+        M = int(input("Mitu kotleti ühel pannil?"))
+        if M>0: break 
+    except ValueError:
+        print("Vale tüüp")
+
+
+#1 {2v}
 z = "   (\_/)\n   (o o)\n   / | \*" #Jänesega muutuja
 arv = int(input("mitu "))
 for i in range(0, 9):
@@ -8,6 +23,31 @@ for i in range(0, 9):
         print(z)
     print()
 
+
+# 1 {1v}
+while True:
+    try:
+        mitu=int(input("Mitu tk: "))
+        if 1 <= mitu < 10:
+            break
+    except ValueError:
+        print("Vale tüüp")
+
+for i in range(mitu):
+    print(' /V\ '.center(10, ' '), end="")
+print()
+for i in range(mitu):
+    print(' /\/\ '.center(10,' '), end="")
+print()
+for i in range(mitu):
+    print(' / V V \ '.center(10,' '), end="")
+print()
+for i in range(mitu):
+    print(' /VV V VV\ '.center(10,' '), end="")
+print()
+
+  
+   
 #2
 sum = 0
 r = int(input("Siseta numbrid"))
@@ -57,3 +97,33 @@ for i in arv:
     urutamine *= int(i)
 
 print(summ, urutamine)
+
+
+# veel:
+
+#5 {5v}
+sum_num = 0
+sum_km = 0
+for i in range(12):
+    num = randint(1000,100000)
+    km = randint(1,1000)
+    sum_num += num  
+    sum_km += km
+    print(f"{i+1}. maakond. \nElanikud: {num}. Pindala: {km}\nKokku: {sum_num}, {sum_km}\n")
+vastus = sum_num / sum_km
+print(f"Keskimne: {vastus:.3f}")
+ 
+# ? {?v}
+N = 25
+kesk1 = 0
+kesk2 = 0
+for i in range(N):
+    h1 = randint(1,5)
+    h2 = randint(1,5)
+    kesk1 += h1
+    kesk2 += h2
+kesk1 /= N 
+kesk2 /= N
+
+print(f"Keskmine hinne 1 klassis on {kesk1}")
+print(f"Keskmine hinne 2 klassis on {kesk2}")
